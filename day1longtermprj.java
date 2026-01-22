3457d32201dcd0fd030bbc4918b2eef7a48e7ac1
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-22 15:30:39 +0530

    updated day 3

--- day1longtermprj.java
+++ day1longtermprj.java
@@ -1,7 +1,11 @@
 public class day1longtermprj {
     public static void main(String args[]) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Task " + i + " Added !");
+        String[] tasks = { "Learn Java basics !", "Understand loops !", "Practice arrays !" };
+        System.out.println("Smart Task Manager");
+        System.out.println("----------------------------------------------------");
+
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.println("Task " + (i + 1) + ": " + tasks[i]);
         }
     }
 }
