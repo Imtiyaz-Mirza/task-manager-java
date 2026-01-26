ac529c2d36c871a4930b728586769c46ff54479c
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-26 23:34:14 +0530

    Updated with asking input with user and created dynamic ArrayList

--- day1longtermprj.java
+++ day1longtermprj.java
@@ -1,26 +1,34 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class day1longtermprj {
-    static void addTask(String tasks) {
-        System.out.println("Task added : " + tasks);
+    static ArrayList<String> tasks = new ArrayList<>();
+
+    static void addTask(String tasksName) {
+        tasks.add(tasksName);
+        System.out.println("Task added Successfully !");
+    }
+
+    static void showTasks() {
+        System.out.println("Your Tasks : ");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+
+        }
     }
 
     public static void main(String args[]) {
-        ArrayList<String> tasks = new ArrayList<>();
-        tasks.add("Learn Java !");
-        tasks.add("Write code and understand it !");
-        tasks.add("Implement in projects !");
-        System.out.println("Smart Task Manager");
-        System.out.println("----------------------------------------------------");
-
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println("Task " + (i + 1) + ": " + tasks.get(i));
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("How many tasks you want to add ? ");
+        int count = userInput.nextInt();
+        userInput.nextLine();
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Enter task " + i + ": ");
+            String task = userInput.nextLine();
+            addTask(task);
         }
-        tasks.remove(0);
-        tasks.set(1, "Implement in projects and submit it on github !");
-        System.out.println("\n Updated Tasks !");
-        for (String every : tasks) {
-            System.out.println(every);
-        }
+        showTasks();
+        System.out.println("Total tasks : " + tasks.size());
     }
+
 }
