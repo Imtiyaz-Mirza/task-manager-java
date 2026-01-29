b71d84c92422ac98c387b2d85111eeb0bdacb3ce
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-29 16:02:05 +0530

    updated with Task manager menu

--- day1longtermprj.java
+++ day1longtermprj.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 public class day1longtermprj {
     static ArrayList<String> tasks = new ArrayList<>();
 
-    static void addTask(String tasksName) {
-        tasks.add(tasksName);
+    static void addTask(Scanner userInput) {
+        System.out.print("Enter task name : ");
+        String task = userInput.nextLine();
+        tasks.add(task);
         System.out.println("Task added Successfully !");
     }
 
     static void showTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks found !");
+            return;
+        }
         System.out.println("Your Tasks : ");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + ". " + tasks.get(i));
@@ -18,19 +24,31 @@ public class day1longtermprj {
     }
 
     public static void main(String args[]) {
-        Scanner userInput = new Scanner(System.in);
-        System.out.print("How many tasks you want to add ? ");
-        int count = userInput.nextInt();
-        userInput.nextLine();
-        for (int i = 1; i <= count; i++) {
-            System.out.println("Enter task " + i + ": ");
-            String task = userInput.nextLine();
-            addTask(task);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n==== Smart Task Manager ====");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option) {
+                case 1:
+                    addTask(sc);
+                    break;
+                case 2:
+                    showTasks();
+                    break;
+                case 3:
+                    System.out.println("Thank you for using Smart Task Manager 🙌");
+                    return;
+                default:
+                    System.out.println("Invalid option. Try again!");
+            }
         }
-        userInput.close();
-        showTasks();
-        System.out.println("Total tasks : " + tasks.size());
-
     }
-
 }
