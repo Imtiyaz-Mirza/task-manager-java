8380cbe618dc6e3f7ff88274b0f5e5da909fed43
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-02-02 16:07:16 +0530

    Add task class with constructor and getter/setter

---
+++ Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    private String title;
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+}
